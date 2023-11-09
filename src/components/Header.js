08e5b92fbbf003976203409b7199b52209176c8f
Oscar Villagran <oscar.villagran@hotmail.com>
2023-11-09 10:47:16 -0500

    Add header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,14 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header className="flex">
+    <header className="flex items-end justify-between px-10 py-2 text-xl text-gray-200 bg-gradient-to-r from-blue-950 to-purple-900">
       <div className="flex">
-        <img src={logo} alt="logo" width="50" />
-        <NavLink to="/">Alpha Gym</NavLink>
+        <NavLink to="/" className="flex items-end gap-4">
+          <img src={logo} alt="logo" width="50" className="text-gray-200" />
+          Alpha Gym
+        </NavLink>
       </div>
-      <div>
+      <div className="flex gap-4">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/contact">Contact</NavLink>
