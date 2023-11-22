07871df873ab0d39fb3459d58419f278a57ed11d
Oscar Villagran <oscar.villagran@hotmail.com>
2023-11-21 23:22:18 -0500

    Add HeroBanner component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,6 +1,27 @@
+import HeroImage from '../assets/images/banner.png';
+
 function HeroBanner() {
   return (
-    <div>HeroBanner</div>
+    <div>
+      <h1>
+        Body & Fitness
+      </h1>
+      <p>
+        Dream, Smile
+        <br />
+        and Exercise.
+      </p>
+      <p>
+        Checkout the most effective exercises.
+      </p>
+      <button type="button">
+        Explore Exercises
+      </button>
+      <p>
+        Exercises
+      </p>
+      <img src={HeroImage} alt="banner" />
+    </div>
   );
 }
 
