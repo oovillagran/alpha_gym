c9c6a0cf140dc446862d8da68045a1af56c14167
Oscar Villagran <oscar.villagran@hotmail.com>
2023-11-22 23:19:29 -0500

    Add hero config

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,25 +2,29 @@ import HeroImage from '../assets/images/banner.png';
 
 function HeroBanner() {
   return (
-    <div>
-      <h1>
-        Body & Fitness
-      </h1>
-      <p>
-        Dream, Smile
-        <br />
-        and Exercise.
-      </p>
-      <p>
-        Checkout the most effective exercises.
-      </p>
-      <button type="button">
-        Explore Exercises
-      </button>
-      <p>
-        Exercises
-      </p>
-      <img src={HeroImage} alt="banner" />
+    <div className="flex">
+      <div>
+        <h1>
+          Body & Fitness
+        </h1>
+        <p>
+          Dream, Smile
+          <br />
+          and Exercise.
+        </p>
+        <p>
+          Checkout the most effective exercises.
+        </p>
+        <button type="button">
+          Explore Exercises
+        </button>
+        <p>
+          Exercises
+        </p>
+      </div>
+      <div>
+        <img src={HeroImage} alt="banner" />
+      </div>
     </div>
   );
 }
