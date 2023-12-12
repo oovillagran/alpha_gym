fb65b3cf9696c6b4c095c6cd750eeae7dd77c4f7
Oscar Villagran <oscar.villagran@hotmail.com>
2023-12-11 23:21:25 -0500

    Add search bar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,13 +1,26 @@
+// import React, { useState, useEffect } from 'react';
+
 function SearchExercises() {
   return (
-    <div className="flex justify-evenly">
-      <p>
-        Filter by Category
+    <>
+      <p className="flex justify-center items-center mt-36 p-20 font-content font-extrabold text-6xl leading-relaxed">
+        Awesome Exercises
+        <br />
+        You Should Know
       </p>
-      <input
-        placeholder="Search by Category"
-      />
-    </div>
+      <div className="flex justify-center items-center px-60">
+        <input
+          className="border border-gray-300 p-2 rounded-md w-full"
+          placeholder="Search by Category"
+          value=""
+          onChange={() => {}}
+          type="text"
+        />
+        <button type="button" className="bg-red-500 text-white py-2 px-8 rounded-md">
+          Search
+        </button>
+      </div>
+    </>
   );
 }
 
