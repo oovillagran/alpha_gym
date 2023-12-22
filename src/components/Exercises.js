73e7d8aee40cac84de17d67f05820b7f76add042
Oscar Villagran <oscar.villagran@hotmail.com>
2023-12-22 01:00:31 -0500

    Add logic for exercise component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,18 @@
-function Exercises() {
+import React, { useEffect, useState } from "react";
+import { exerciseOptions, fetchData } from "./fetchData";
+
+
+function Exercises({ exercises, setExercises, bodyPart })  {
+  console.log(exercises);
+
   return (
     <>
-      <div>Exercises</div>
+      <h3 id="exercises" className="mb-44">Showing Results</h3>
+      <ul>
+        {exercises.map((exercise, index) => (
+        <li key={index}>{exercise.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
