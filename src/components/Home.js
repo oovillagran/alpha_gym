1ebfadccba9e8e3900c0b6c24bc316b4e8c92ac5
Oscar Villagran <oscar.villagran@hotmail.com>
2024-01-15 18:05:20 -0500

    Add console log statemet deletion

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,11 @@ import Exercises from './Exercises';
 function Home() {
   const [bodyPart, setBodyPart] = useState('all');
   const [exercises, setExercises] = useState([]);
-  console.log(bodyPart);
-  // const [currentPage, setCurrentPage] = useState(1);
 
   return (
     <div>
       <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       <Exercises exercises={exercises} setExercises={setExercises} bodyPart={bodyPart} />
-      {/* <Exercises
-        exercises={exercises}
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-      /> */}
     </div>
   );
 }
