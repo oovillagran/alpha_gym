823e0c25d9919e33f2b0e3e6a922642f576fd7f2
Oscar Villagran <oscar.villagran@hotmail.com>
2024-01-19 22:53:11 -0500

    Set Exercises Detail Use Effect

--- src/pages/ExercisesDetail.js
+++ src/pages/ExercisesDetail.js
@@ -1,15 +1,30 @@
 // import React, { useEffect, useState } from 'react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import { useParams } from 'react-router';
 // import { exerciseOptions, fetchData } from '../components/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
+import { useParams } from 'react-router';
+import { exerciseOptions } from '../components/fetchData';
 
 function ExercisesDetail() {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fecthExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetail);
+    }
+  }, [id]);
+
   return (
     <div>
-      <Detail />
+      <Detail exerciseDetail = {exerciseDetail} />
       <ExerciseVideos />
       <SimilarExercises />
     </div>
